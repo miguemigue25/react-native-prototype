671fcaa76db0f9b6b2a4f50be4cf878598147934
Miguel <migueortiz0125@gmail.com>
2024-05-29 12:56:41 -0700

    create screen completed

--- aora/lib/appwrite.js
+++ aora/lib/appwrite.js
@@ -1,4 +1,4 @@
-import { Account, Avatars, Client, Databases, ID, Query } from 'react-native-appwrite';
+import { Account, Avatars, Client, Databases, ID, Query, Storage } from 'react-native-appwrite';
 
 
 
@@ -13,14 +13,15 @@ client
 const account = new Account(client);
 const avatars = new Avatars(client);
 const databases = new Databases(client);
+const storage = new Storage(client);
 
 // Register User
 export const createUser = async (email, password, username) => {
     try {
         const newAccount = await account.create(
             ID.unique(),
-            email, 
-            password, 
+            email,
+            password,
             username
         )
         if (!newAccount) throw Error;
@@ -35,7 +36,7 @@ export const createUser = async (email, password, username) => {
             ID.unique(),
             {
                 accountId: newAccount.$id,
-                email: email, 
+                email: email,
                 username: username,
                 avatar: avatarUrl
             }
@@ -53,7 +54,7 @@ export const signIn = async (email, password) => {
         await account.deleteSession("current");
         const session = await account.createEmailPasswordSession(email, password);
 
-        return session;        
+        return session;
     } catch (error) {
         throw new Error(error);
     }
@@ -76,7 +77,7 @@ export const getCurrentUser = async () => {
 
         return currentUser.documents[0];
     } catch (error) {
-        console.log(error);        
+        console.log(error);
     }
 }
 
@@ -87,21 +88,88 @@ export const getCurrentUser = async () => {
 export const signOut = async () => {
     try {
         const session = await account.deleteSession("current");
-        
+
         return session;
     } catch (error) {
-        throw new Error(error);        
+        throw new Error(error);
     }
 }
 
 
 // upload File
+export const uploadFile = async (file, type) => {
+    if (!file) return;
 
+    const { mimeType, ...rest } = file;
+    const asset = { type: mimeType, ...rest };
+
+    try {
+        const uploadFile = await storage.createFile(
+            appwriteConfig.storageId,
+            ID.unique(),
+            asset
+        );
+        const fileUrl = await getFilePreview(uploadFile.$id, type);
+
+        return fileUrl;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
 
 // Get File Preview
+export const getFilePreview = async (fileId, type) => {
+    let fileUrl;
 
+    try {
+        if (type === "video") {
+            fileUrl = storage.getFilePreview(appwriteConfig.storageId, fileId)
+        } else if (type === "image") {
+            fileUrl = storage.getFilePreview(
+                appwriteConfig.storageId,
+                fileId,
+                2000,
+                2000,
+                "top",
+                100
+            );
+        } else {
+            throw new Error("Invalid file type");
+        }
+        if (!fileUrl) throw Error;
+
+        return fileUrl;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
 
 // Create Video Post
+export const createVideo = async (form) => {
+    try {
+        const [thumbnailUrl, videoUrl] = await Promise.all([
+            uploadFile(form.thumbnail, "image"),
+            uploadFile(form.video, "video"),
+        ])
+
+        const newPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.videoCollectionId,
+            ID.unique(),
+            {
+                title: form.title,
+                thumbnail: thumbnailUrl,
+                video: videoUrl,
+                prompt: form.prompt,
+                creator: form.userId
+            }
+        )
+
+        return newPost;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
 
 
 // Get All Video Posts
